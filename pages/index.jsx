d5c1815998549eb51ff2272c2b7c7a6177e63d62
Bruno Patrício <bpatriciocosta@gmail.com>
2022-11-22 15:42:01 -0300

    Background color

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,7 +3,12 @@ import styled from 'styled-components'
 import BackgroundImageWithSpace from '../src/components/layout/backgroundImageWithChildren/BackgroundImageWithChildren'
 
 const StyledDiv = styled.div`
-  background-color: ${(props) => props.theme.colors.primary};
+  background-color: ${(props) => props.theme.colors.white};
+  min-width: 500px;
+
+  @media (max-width: 500px) {
+    min-width: 100%;
+  }
 `
 
 export default function homePage() {
