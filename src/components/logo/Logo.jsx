37f012da06adb35e5b5c8453eca31e65c411f74f
Bruno Patrício <bpatriciocosta@gmail.com>
2022-11-23 12:39:57 -0300

    Ajuste de responsividade do logo

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -2,11 +2,20 @@
 /* eslint-disable @next/next/no-img-element */
 import styled from 'styled-components'
 
-const Image = styled.img``
+const Image = styled.img`
+  max-width: 250px;
+
+  @media (max-width: 768px) {
+    width: 80%;
+  }
+`
 
 const VerticalText = styled.h1`
   font-size: 80px;
   color: ${(props) => props.theme.colors.black};
+  line-height: 70px;
+  margin-left: 150px;
+  margin-top: -80px;
 `
 
 const PrimaryColor = styled.span`
