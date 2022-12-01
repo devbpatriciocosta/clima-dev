1746026466c00d5f1a814c6439cf79b5909474dc
devBruno <bpatriciocosta@gmail.com>
2022-12-01 11:47:02 -0300

    Criando search city

---
+++ src/components/searchCity/SearchCity.jsx
@@ -0,0 +1,5 @@
+import InputWithSuggestions from '../form/inputWithSuggestions/InputWithSuggestions'
+
+export default function SearchCity() {
+  return <InputWithSuggestions placeholder="Digite o nome da cidade" />
+}
