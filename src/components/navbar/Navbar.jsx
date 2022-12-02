222a1d9c881ddbc5334c4d3bee6972a2834a1b10
Bruno Patrício <bpatriciocosta@gmail.com>
2022-12-02 14:16:04 -0300

    Criado NavBar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+import Logo from '../logo/Logo'
+
+const styledNavBar = styled.div``
+
+export default function Navbar() {
+  return (
+    <styledNavBar>
+      <Logo />
+    </styledNavBar>
+  )
+}
