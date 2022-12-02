90732796664a6245b90fba489fc1b71e0b00eacf
Bruno Patrício <bpatriciocosta@gmail.com>
2022-12-02 14:16:35 -0300

    Criado dynamic rout

--- pages/info/[lat]/[lon].jsx
+++ pages/info/[lat]/[lon].jsx
@@ -1,9 +1,17 @@
+import styled from 'styled-components'
 
+import Navbar from '../../../src/components/navbar/Navbar'
 
-export default function infoPage() { 
-    return (
-        <div>
-            Informações climáticas
-        </div>
-    )
+const Body = styled.div`
+  background-color: ${(props) => props.theme.colors.grey};
+  min-height: 100vh;
+`
+
+export default function infoPage() {
+  return (
+    <>
+      <Navbar />
+      <Body>Informações climáticas</Body>
+    </>
+  )
 }
