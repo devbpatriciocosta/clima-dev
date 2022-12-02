6d02610787c47738d5ced50ef2aacebe1ccff92e
Bruno Patrício <bpatriciocosta@gmail.com>
2022-12-02 14:39:27 -0300

    Adding info / conteúdo

--- pages/info/[lat]/[lon].jsx
+++ pages/info/[lat]/[lon].jsx
@@ -8,12 +8,33 @@ const Body = styled.div`
   min-height: 100vh;
 `
 
+const Content = styled.div`
+  padding: 80px 0;
+`
+
+const Text = styled.p`
+  font-size: 20px;
+  padding: 0;
+  margin: 0;
+`
+
+const CityName = styled.h2`
+  font-size: 60px;
+  padding: 0;
+  margin: 0;
+`
+
 export default function infoPage() {
   return (
     <>
       <Navbar />
       <Body>
-        <Container>Informações climáticas</Container>
+        <Container>
+          <Content>
+            <Text>Previsão do tempo para:</Text>
+            <CityName>Osório, RS, Brasil</CityName>
+          </Content>
+        </Container>
       </Body>
     </>
   )
